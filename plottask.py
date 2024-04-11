e6ddfeb6eb00bde1195370c89cce4d67c12c8779
Filipe Carvalho <filipeferc88@gmail.com>
2024-04-11 01:56:49 +0100

    Cleaned and organized the comments in plottask.py

--- plottask.py
+++ plottask.py
@@ -1,45 +1,38 @@
-# This program prints a histogram of a normal distribution
-# of a 1000 values with a mean of 5 and standard deviation of 2
-# and a plot of the function h(x)=x**3 in the range 0 to 10
+# Program plottask.py
+# This program generates and plot a histogram representing a normal distribution
+# with 1000 values (mean=5, std dev=2)
+# and a line of the function h(x)=x^3 over the range (0 to 10].
 # Author: Filipe Carvalho
 
-# Import Numpy and MatPlotLib.Pyplot.
+# Import required libraries
 import numpy as np
 import matplotlib.pyplot as plt
 
-# Set up the seed for random to 1 to avoid a new random number to be generated everytime this program is run
+# Set random seed to be reproduced with same value
 np.random.seed(1) 
-# Generate the randome number using numpy
-normalRand = np.random.normal(loc=5, scale = 2, size = 1000)
-# print(normalRand)
+# Generate a random sample of 1000 values from a normal distribution
+sample= np.random.normal(loc=5, scale = 2, size = 1000)
 
 
-# Plot the histogram using the random data that was generated and formatting with colour yellow and label
-# Add title, axis labels and legend
-# Add bin to provide clearer visual representation of the normal distribution
-# Add alpha to give a slight transparency to the histrogram
-# and help distinguish the line plot
-plt.hist(normalRand, bins=30, color='y', alpha=0.7, label="Normal Distribution")
+
+# Plot the histogram of the samples
+plt.hist(sample, bins=30, color='y', alpha=0.7, label="Normal Distribution")
 plt.title("Normal Distribution and Function Plot")
 plt.xlabel("Value")
 plt.ylabel("Occurrence")
 plt.legend()
 
 
-# generate an array within a range from 1 to 10
-# rande (11) was added to attend to this requirement
-# as in range (1,10) the number 10 is not inclusive.
+# generate an array of x values within range (1 - 10]
 x_points = np.array(range(11))
-# add the numbers from the array to the power of 3
-h_points = x_points ** 3 # y is equal x to the power of 3
+# calculate h(x) = x ^ 3 for each x value
+h_points = x_points ** 3
 
-# Plot on the same chart a line
-# with the numbers from the array in the x axis and 
-# the second array with numbers calculated to the power of 3 in the y axis 
-# format with colour blue and label
-# Add legend
+# Plot of the function h(x) = x ^ 3 on same graphic
 plt.plot(x_points, h_points, color='b',label = "h(x) = x ^ 3")
 plt.legend()
+
+# Display the graph with histogram and line plot
 plt.show()
 
 # End of the Program
